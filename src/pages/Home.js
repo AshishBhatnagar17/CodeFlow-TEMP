90a826cb1f4e10c124a1cfc9379300ea0bbd6430
Ashish Bhatnagar <121173156+AshishBhatnagar17@users.noreply.github.com>
2024-07-27 01:24:49 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -74,7 +74,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="https://www.linkedin.com/in/ashish-bhatnagar-2a9877222/">Ashish Bhatnagar</a>
                 </h4>
             </footer>
         </div>
